3aa8b921c6b58b101d8b3ceb7e52cf0e2b5be377
ungvas <ungvas@gmail.com>
2015-10-26 18:10:37 +0200

    add exchange rates components

--- app/src/main/java/md/fusionworks/lifehack/ui/widget/CurrencyView.java
+++ app/src/main/java/md/fusionworks/lifehack/ui/widget/CurrencyView.java
@@ -2,30 +2,51 @@ package md.fusionworks.lifehack.ui.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.Checkable;
-import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import md.fusionworks.lifehack.R;
 
 /**
  * Created by ungvas on 10/23/15.
  */
-public class CurrencyView extends FrameLayout implements Checkable{
+public class CurrencyView extends TypefaceTextView {
+
+    private static final int BG_CURRENCY_VIEW_STATE_NORMAL = R.drawable.bg_currency_view_state_normal;
+    private static final int BG_CURRENCY_VIEW_STATE_SELECTED = R.drawable.bg_currency_view_state_selected;
+
+    private int bgCurrencyViewResId;
+
+    public CurrencyView(Context context) {
+        super(context, null, R.attr.currencyViewStyle);
+
+        initialize();
+    }
 
     public CurrencyView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        super(context, attrs, R.attr.currencyViewStyle);
+
+        initialize();
+    }
+
+    private void initialize() {
+
+        setSelected(false);
+    }
+
+    public void setSelected(boolean selected) {
+
+        setBackgroundResource(selected ? BG_CURRENCY_VIEW_STATE_SELECTED : BG_CURRENCY_VIEW_STATE_NORMAL);
+    }
+
+    public boolean isSelected() {
+
+        return bgCurrencyViewResId == BG_CURRENCY_VIEW_STATE_SELECTED;
     }
 
     @Override
-    public void setChecked(boolean checked) {
+    public void setBackgroundResource(int resid) {
+        super.setBackgroundResource(resid);
 
-    }
-
-    @Override
-    public boolean isChecked() {
-        return false;
-    }
-
-    @Override
-    public void toggle() {
-
+        bgCurrencyViewResId = resid;
     }
 }
