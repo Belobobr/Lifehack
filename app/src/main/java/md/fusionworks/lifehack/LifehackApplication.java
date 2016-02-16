85bc775bb1d3cda0c47ad0ed7614c3259405214f
ungvas <ungvas@gmail.com>
2016-02-16 13:53:00 +0200

    install leak canary

--- app/src/main/java/md/fusionworks/lifehack/LifehackApplication.java
+++ app/src/main/java/md/fusionworks/lifehack/LifehackApplication.java
@@ -1,6 +1,7 @@
 package md.fusionworks.lifehack;
 
 import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by ungvas on 10/20/15.
@@ -9,5 +10,6 @@ public class LifehackApplication extends Application {
 
   @Override public void onCreate() {
     super.onCreate();
+    LeakCanary.install(this);
   }
 }
